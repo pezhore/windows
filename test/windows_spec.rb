9de33472263fa196675e30f3adfa83b8743cf2f6
pezhore <pezhore@gmail.com>
2020-07-26 17:35:48 -0500

    cleaning

--- test/windows_spec.rb
+++ test/windows_spec.rb
@@ -1,7 +1,8 @@
 require_relative 'spec_helper'
 
-describe 'windows box' do
-  it 'should have a vagrant user' do
-    expect(user 'vagrant').to exist
+set :backend, :winrm
+set :os, :family => 'windows'
+
+describe user('vagrant') do
+  it { should exist }
   end
-end
